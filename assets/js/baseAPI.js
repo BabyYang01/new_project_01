a58b00f0468195db8000680e3f61cdcfbd228d98
zhaodanyang <610567955@qq.com>
2022-05-05 14:40:21 +0800

    again

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,5 @@
 // 注意：每次调用 $.get()$.post 或$.ajax() 的时候 会先调用ajaxprefilter这个函数
-// 在这个函数中，可以拿到我们给Ajax 提供的配置对象
+// 在这个函数中，可以拿到我们给Ajax 提供的配置对象v
 
 $.ajaxPrefilter(function(options){
     console.log(options.url)
